7ffb56e4e30f852b8e0a29f9e53d93fa4e8a0758
Naslin <naslinrahman@hotmail.com>
2020-08-23 00:36:04 -0400

    added contact page

---
+++ client/src/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>hey</h1>
+            <input type ="text"></input>
+            <input type="submit" className="submit-btn" value="Submit"/>
+        </div>
+    );
+};
+
+export default Contact;
