b94e6606daeb066800aca024a40a7cb9a17ed79f
Naslin <naslinrahman@hotmail.com>
2020-08-30 17:03:28 -0400

    fixed style and indentation of contact page

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -5,28 +5,28 @@ import Footer from "./Footer";
 
 const Contact = () => {
   return (
-<div>
-<Navbar/>    
-<div className="contact-container">
-      <form action="mailto:SkillsMarketPlace.org" method="post" enctype="text/plain">
-        <h1>Contact Us</h1>
-        <p>
+    <div>
+      <Navbar/>    
+      <div className="contact-container">
+        <form action="mailto:SkillsMarketPlace.org" method="post" enctype="text/plain">
+          <h1>Contact Us</h1>
+          <p>
           Interested about our services? Contact us today! We are here to help,
-          email us at info@SkillsMarketPlace.org or us the form below to get in
+          email us at info@SkillsMarketPlace.org or contact us the form below to get in
           touch.
-        </p>
-        <hr />
-        <input type="text" name="yourName" placeholder=" Name *"></input>
-        <br></br>
-        <input type="text" name="yourEmail" placeholder="Email *"></input>
-        <br/>
-        <textarea name="yourMessage" rows="7" cols="100" placeholder="Message *"></textarea>
-        <br></br>
-        <button className="submit-btn" type="submit">Submit</button>
-      </form>
+          </p>
+          <hr />
+          <input type="text" name="yourName" placeholder=" Name *"></input>
+          <br></br>
+          <input type="text" name="yourEmail" placeholder="Email *"></input>
+          <br/>
+          <textarea name="yourMessage" rows="7" cols="100" placeholder="Message *"></textarea>
+          <br></br>
+          <button className="submit-btn" type="submit">Submit</button>
+        </form>
+      </div>
+      <Footer/>
     </div>
-<Footer/>
-</div>
   );
 };
 
